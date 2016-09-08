7f0eb33d0f9fdf2db8513d2b86a2fc6557cf6e92
rerafyaw <anderson_nov@hotmail.com>
2016-09-08 12:44:59 -0400

    Turning in my assignment for Day 1 - apologies for the 40 minute downtime, i accidentally corrupted my entire Git package and had to google on how to restore it

--- day2/hw1.rkt
+++ day2/hw1.rkt
@@ -1,42 +1,48 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: Anderson Ang Wei Jian
 ;;;
 ;;; Check one:
 ;;; [ ] I completed this assignment without assistance or external resources.
-;;; [ ] I completed this assignment with assistance from ___
-;;;     and/or using these external resources: ___
+;;; [Y] I completed this assignment with assistance from ___
+;;;     and/or using these external resources: stackoverflow (examples 5 and 6)
 
 ;;     NOTE:  You need not worry about error checking in the programs below.
 
 ;; 1.  WRITE SQUARE:  given n, returns n^2.  Hint:  use *
 (define (square x)
-  your-code-here)
-  
+  (* x x))
+
 (display (square 2)) (newline)  ;; -> 4
 
 ;; 2.  WRITE is-right-triangle:  given three numbers, returns true iff the third
 ;;     could be the hypotenuse of a right triangle with the specified three side lengths
 ;;     Hint:  use = to compare numeric values
 
-(define (is-right-triangle)
-  your-code-here)
-  
+(define (is-right-triangle x y z)
+  (cond ((= (* z z) (+ (* x x)(* y y) )) true)
+        (else false)))
+
 (display (is-right-triangle 3 4 5)) (newline)  ;; -> #t
 (display (is-right-triangle 4 5 6)) (newline)  ;; -> #f
 
 ;; 3.  WRITE FACTORIAL:  given n, returns n!
 ;;     Hint:  recursion is your friend
 (define (fact n)
-  your-code-here)
+    (if(= n 0)
+            1
+     (* n (fact (- n 1)))))
 
 (display (fact 1)) (newline) ;; -> 1
-(display (fact 2)) (newline) ;; -> 1
+(display (fact 2)) (newline) ;; -> 2
 
 ;; 4.  WRITE FIBONACCI:  given n, returns the nth fibonacci number as shown below
 ;;     Hint:  don't run this on really big numbers!
 (define (fib n)
-   your-code-here)
+  (if (<= n 2)
+    1
+    (+ (fib (- n 1)) (fib (- n 2)))))
+
 
 (display (fib 1)) (newline) ;; -> 1
 (display (fib 2)) (newline) ;; -> 1
@@ -48,7 +54,11 @@
 ;; 5.  WRITE a procedure that takes a list of numbers and returns the sum of those numbers
 ;;     Hint:  first, rest, cons
 (define (sum lst)
-  your-code-here)
+  (let loop ((lst lst) (accum 0))
+  (cond
+    ((empty? lst) accum)
+    ((not (number? (car lst))) '())
+    (else (loop (cdr lst) (+ accum (car lst)))))))
 
 (display (sum '(1 2 3 4))) (newline) ;; -> 10
 (display (sum '(1 20 300))) (newline) ;; -> 321
@@ -56,6 +66,12 @@
 ;; 6.  WRITE a procedure that takes a list of numbers and returns the largest one.
 ;;     While there are solutions using scheme's built-in max, we were actually hoping you'd do something else...
 (define (my-max lst)
-  your-code-here)
+      (let loop ((lst (cdr lst))
+      (maxval (car lst)))  ; first val maximum
+      (cond ((null? lst) maxval)    ; null scenario
+            ((> (car lst) maxval)   ; current element > max = max
+             (loop (cdr lst) (car lst))) ; replace new maximum
+            (else
+             (loop (cdr lst) maxval)))))
 
 (display (my-max '(1 10 2 20 3))) (newline) ;; -> 20
